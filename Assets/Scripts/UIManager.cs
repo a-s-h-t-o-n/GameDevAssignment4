691a6e3372477908728aaff8ce9a219e1c4c59bc
Ashton <ashton.king@student.uts.edu.au>
2023-11-03 09:18:02 +1100

    added hud

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,7 +8,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+
     }
 
     // Update is called once per frame
@@ -20,5 +20,10 @@ public class UIManager : MonoBehaviour
     public void LoadLevelOne()
     {
         SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+    }
+
+    public void LoadTitleScreen()
+    {
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
     }
 }
